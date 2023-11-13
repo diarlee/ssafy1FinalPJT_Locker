94f81d98b70fc2f3129b7f70b7bb4b8c6872a4ca
전민정 <jeonminjeong@jeonminjeong-ui-MacBookAir.local>
2023-11-13 20:12:06 +0900

    description 변경

--- ssafit_project_이구범_전민정/src/main/java/com/ssafy/ssafit/config/SwaggerConfig.java
+++ ssafit_project_이구범_전민정/src/main/java/com/ssafy/ssafit/config/SwaggerConfig.java
@@ -21,6 +21,6 @@ public class SwaggerConfig {
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title("SSAFY 10기 BOARD REST API")
-				.description("엄청나게 위대한 전민정이 생각하고 하찮은 이구범이 키보드 딸깍해서 만든 서버 입니다.").version("0.1").build();
+				.description("이병 이병수!\n 넵 !!!").version("0.1").build();
 	}
 }
