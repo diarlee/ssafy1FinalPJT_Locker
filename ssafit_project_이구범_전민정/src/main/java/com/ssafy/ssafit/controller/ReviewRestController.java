ab887e55a95e5583188ece15181e7f61de3e8c00
전민정 <jeonminjeong@jeonminjeong-ui-MacBookAir.local>
2023-11-14 16:58:50 +0900

    리뷰 업데이트 수정

--- ssafit_project_이구범_전민정/src/main/java/com/ssafy/ssafit/controller/ReviewRestController.java
+++ ssafit_project_이구범_전민정/src/main/java/com/ssafy/ssafit/controller/ReviewRestController.java
@@ -27,16 +27,15 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/api")
 public class ReviewRestController {
-	
+
 	@Autowired
 	private UserService userService;
 
 	@Autowired
 	private ReviewService reviewService;
-	
-	
+
 	@GetMapping("/reviewAll")
-	@ApiOperation(value="모든 리뷰 조회", notes="모든 리뷰 조회하기")
+	@ApiOperation(value = "모든 리뷰 조회", notes = "모든 리뷰 조회하기")
 	public ResponseEntity<?> getAllList() {
 		List<Review> list = reviewService.getAll();
 		if (list == null || list.size() == 0) {
@@ -47,7 +46,7 @@ public class ReviewRestController {
 	}
 
 	@GetMapping("/review/{videoId}")
-	@ApiOperation(value="리뷰 조회", notes="영상 아이디에 해당하는 모든 리뷰 조회하기")
+	@ApiOperation(value = "리뷰 조회", notes = "영상 아이디에 해당하는 모든 리뷰 조회하기")
 	public ResponseEntity<?> getList(@PathVariable String videoId) {
 		List<Review> list = reviewService.getList(videoId);
 		if (list == null || list.size() == 0) {
@@ -56,17 +55,15 @@ public class ReviewRestController {
 			return new ResponseEntity<List<Review>>(list, HttpStatus.OK);
 		}
 	}
-	
-	
 
 	@GetMapping("/review/detail/{id}")
-	@ApiOperation(value="리뷰 상세 조회", notes="리뷰 상세 조회하기")
+	@ApiOperation(value = "리뷰 상세 조회", notes = "리뷰 상세 조회하기")
 	public ResponseEntity<?> getReview(@PathVariable int id) {
 		Review review = reviewService.getReview(id);
 		if (review == null) {
 			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 		} else {
-			if(id == review.getReviewId()) {
+			if (id == review.getReviewId()) {
 				int viewCnt = review.getViewCnt();
 				reviewService.modifyViewCnt(viewCnt, id);
 			}
@@ -75,33 +72,37 @@ public class ReviewRestController {
 	}
 
 	@PostMapping("/review/write")
-	@ApiOperation(value="리뷰 등록", notes="리뷰 작성하기")
+	@ApiOperation(value = "리뷰 등록", notes = "리뷰 작성하기")
 	public ResponseEntity<?> writeReview(@RequestBody Review review) {
 //		User user = (User) session.getAttribute("loginUser");
 //		if (user != null) {
 //			review.setVideoId(videoId);
 //			review.setUserId(user.getUserId());
-		
-			User user = userService.getUser(review.getUserId());
-			review.setWriter(user.getUsername());
-			
-			reviewService.writeReview(review);
-			return new ResponseEntity<Void>(HttpStatus.CREATED);
+
+		User user = userService.getUser(review.getUserId());
+		review.setWriter(user.getUsername());
+
+		reviewService.writeReview(review);
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
 //		} else {
 //			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 //		}
 	}
 
+	@PutMapping("/review/modify/{id}")
+	@ApiOperation(value = "리뷰 수정", notes = "리뷰 수정하기")
+	public ResponseEntity<?> modifyReview(Review review, @PathVariable int id) {
 
-	@PutMapping("/review/modify")
-	@ApiOperation(value="리뷰 수정", notes="리뷰 수정하기")
-	public ResponseEntity<?> modifyReview(Review review) {
-		reviewService.modifyReview(review);
-		return new ResponseEntity<Void>(HttpStatus.OK);
+		if (review.getReviewId() == id) {
+			reviewService.modifyReview(review);
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		}
 	}
 
 	@DeleteMapping("/review/{id}")
-	@ApiOperation(value="리뷰 삭제", notes="리뷰 삭제하기")
+	@ApiOperation(value = "리뷰 삭제", notes = "리뷰 삭제하기")
 	public ResponseEntity<?> removeReview(@PathVariable int id) {
 		reviewService.removeReview(id);
 		return new ResponseEntity<Void>(HttpStatus.OK);
