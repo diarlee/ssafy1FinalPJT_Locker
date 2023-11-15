62dc07c3530c58414d27e94f27ead65844e87778
Gardener-Soul <str-pes@naver.com>
2023-11-15 17:58:15 +0900

    달력 component 추가

--- SSAFIT_VUEPJT/src/App.vue
+++ SSAFIT_VUEPJT/src/App.vue
@@ -1,9 +1,5 @@
 <template>
     <Header></Header>
-  <!-- <RouterLink :to="{ name: 'videoDetail' }">videoDetail</RouterLink>
-  <RouterLink :to="{ name: 'reviewCreate' }">CreateReview</RouterLink> -->
-  <!-- <RouterLink :to="{ name: 'reviewDetail' }">DetailReview</RouterLink> -->
-  <!-- <RouterLink :to="{ name: 'reviewUpdate' }">UpdateReview</RouterLink> -->
   <RouterView></RouterView>
 </template>
 
