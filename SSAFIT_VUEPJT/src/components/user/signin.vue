b57511f2abb013784ec75feb1aa2f937a12756b1
전민정 <jeonminjeong@jeonminjeong-ui-MacBookAir.local>
2023-11-15 16:30:40 +0900

    test

---
+++ SSAFIT_VUEPJT/src/components/user/signin.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <div>회원가입 페이지 입니다.</div>
+    <div>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+        crossorigin="anonymous"
+      />
+    </div>
+    <div>
+      <div class="container">
+        <div class="shadow pt-5 pb-5 m-5 bg-light rounded" style="width: 80%">
+          <form class="reviewform" method="post" action="main" style="width: 80%; margin: auto">
+            <div class="text-center">
+              <span class="material-symbols-outlined">dialogs</span>
+              <h2 class="d-inline mb-3 fs-4 fw-semibold">회원가입</h2>
+              <span class="material-symbols-outlined">dialogs</span>
+            </div>
+            <hr class="mb-3" />
+
+            <div class="mb-3">
+              <label for="userId" class="col-form-label">아이디</label>
+              <input type="text" class="form-control" id="userId" v-model="userId" />
+            </div>
+            <div class="mb-3">
+              <label for="password" class="col-form-label">비밀번호</label>
+              <input type="text" class="form-control" id="password" name="title" v-model="password" />
+            </div>
+            <div class="mb-3">
+              <label for="username" class="col-form-label">이름</label>
+              <input type="text" class="form-control" id="username" name="title" v-model="username" />
+            </div>
+            <div class="mb-3">
+              <label for="email" class="col-form-label">이메일</label>
+              <input type="text" class="form-control" id="email" name="title" v-model="email" />
+            </div>
+
+            <!-- <RouterLink class="w-20 me-2 btn btn-outline-primary" :to="{ name: 'login' }" @click="signin">회원가입</RouterLink> -->
+            <button class="w-20 me-2 btn btn-outline-primary" @click="signin">회원가입</button>
+            <RouterLink class="w-20 btn btn-outline-danger" :to="{ name: 'home' }">취소</RouterLink>
+            <!-- <button type="button" onclick="location.href='./main?act=reviewList&videoId=${videoId}&url=${url }'" class="w-20 btn btn-outline-danger">취소</button> -->
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { RouterView, RouterLink } from "vue-router";
+import { ref } from "vue";
+import { useUsersStore } from "@/stores/users";
+const store = useUsersStore();
+
+const userId = ref("");
+const password = ref("");
+const username = ref("");
+const email = ref("");
+
+const signin = () => {
+  // console.log("ing")
+  store.signinFunc(userId.value, password.value, username.value, email.value);
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+
+a {
+  text-decoration: none !important;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  left: 0;
+  right: 0;
+  margin: auto;
+  margin-top: 100px;
+  margin-bottom: 200px;
+  width: 1400px;
+}
+
+.container-fluid {
+  padding: 0 20px 0 20px !important;
+}
+
+#page-name {
+  font-weight: bold;
+  font-size: 2rem;
+}
+
+.main-image {
+  width: 100%;
+  height: 200px;
+}
+
+.search-box {
+  width: 100%;
+  padding: 10px;
+}
+
+input {
+  width: 100%;
+}
+
+.video-list img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+
+.video-box {
+  width: 100%;
+}
+</style>
---
+++ SSAFIT_VUEPJT/src/components/video/VideoDetail.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="container">
+    <div class="shadow pt-5 pb-5 m-5 bg-light rounded" style="width: 80%">
+      <form class="reviewform" style="width: 80%; margin: auto">
+        <div class="text-center">
+          <span class="material-symbols-outlined">dialogs</span>
+          <h2 class="d-inline mb-3 fs-4 fw-semibold">운동영상 리뷰 목록</h2>
+          <span class="material-symbols-outlined">dialogs</span>
+        </div>
+
+        <hr class="mb-3" />
+        <!-- 임시 src -->
+        <div class="text-center">
+          <iframe
+            width="560"
+            height="315"
+            :src="route.params.url"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+
+        <hr class="mb-3" />
+
+        <div class="d-flex justify-content-between">
+          <button
+            type="button"
+            @click="goToCreateReview"
+            class="w-20 me-2 btn btn-outline-primary"
+          >
+            글 작성
+          </button>
+          <div
+            style="border-bottom: solid gray 1px"
+            class="d-flex align-items-center"
+          >
+            <span class="material-symbols-outlined align-items-center"
+              >search</span
+            >
+
+            <input
+              style="border: none; background-color: transparent"
+              placeholder="제목,내용으로 검색"
+            />
+          </div>
+        </div>
+
+        <hr class="mb-3" />
+
+        <div class="d-flex justify-content-center">
+          <table style="width: 95%">
+            <tr>
+              <th class="border-bottom p-3">번호</th>
+              <th class="border-bottom p-3">제목</th>
+              <th class="border-bottom p-3">작성자</th>
+              <th class="border-bottom p-3">조회수</th>
+              <th class="border-bottom p-3">작성시간</th>
+            </tr>
+            <div v-for="review in reviewList">
+              <tr
+                @click="goToReviewDetail"
+                style="cursor: pointer; text-decoration: underline"
+                class="link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"
+              >
+                <td class="p-3">{{ review.reviewId }}</td>
+                <td class="p-3">{{ review.title }}</td>
+                <td class="p-3">{{ review.writer }}</td>
+                <td class="p-3">{{ review.viewCnt }}</td>
+                <td class="p-3">{{ review.regDate }}</td>
+              </tr>
+            </div>
+
+            <!-- <c:forEach items="${reviewList}" var="review">
+              <tr
+                onclick="location.href='./main?act=reviewDetail&videoId=${videoId}&url=${url }&reviewId=${review.reviewId }'"
+                style="cursor: pointer; text-decoration: underline"
+                class="link-dark link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"
+              >
+                <td class="p-3">${review.reviewId }</td>
+                <td class="p-3">${review.title }</td>
+                <td class="p-3">${review.writer }</td>
+                <td class="p-3">${review.viewCnt }</td>
+                <td class="p-3">${review.regDate }</td>
+              </tr>
+            </c:forEach> -->
+          </table>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+import router2 from "@/router";
+import { ref } from "vue";
+
+const route = useRoute();
+const router = useRouter();
+
+onMounted(() => {
+    store.getReviewList(route.params.videoId)
+})
+
+const goToCreateReview = function () {
+  // console.log("ing");
+  router2.push({ name: "reviewCreate" });
+};
+
+const goToReviewDetail = function () {
+  router2.push({ name: "reviewDetail", param: { id: 1 } });
+};
+
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+
+a {
+  text-decoration: none !important;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  left: 0;
+  right: 0;
+  margin: auto;
+  margin-top: 100px;
+  margin-bottom: 200px;
+  width: 1400px;
+}
+
+#page-name {
+  font-weight: bold;
+  font-size: 2rem;
+}
+
+input {
+  width: 100%;
+}
+</style>
