216a4693363e6301df7c9617be8c3aa4ac6bcca6
전민정 <jeonminjeong@jeonminjeong-ui-MacBookAir.local>
2023-11-20 14:21:02 +0900

    다시 수정 ㅜㅜ

--- ssafit_project_이구범_전민정/src/main/java/com/ssafy/ssafit/controller/ArticleRestController.java
+++ ssafit_project_이구범_전민정/src/main/java/com/ssafy/ssafit/controller/ArticleRestController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -88,7 +89,7 @@ public class ArticleRestController {
 	
 	@PostMapping("/write")
 	@ApiOperation(value = "게시글 등록")
-	public ResponseEntity<?> createArticle(Article article, @RequestParam(required = true) MultipartFile image) throws IOException{
+	public ResponseEntity<?> createArticle(@RequestBody Article article, @RequestParam(required = true) MultipartFile image) throws IOException{
 		User user = userService.getUser(article.getUserId());
 		if(user != null) {
 			article.setWriter(user.getUsername());
@@ -102,7 +103,7 @@ public class ArticleRestController {
 	
 	@PutMapping("/modify/{articleId}")
 	@ApiOperation(value="게시글 수정")
-	public ResponseEntity<?> updateArticle(@PathVariable int articleId, Article article, @RequestParam(required = true) MultipartFile image) throws IOException{
+	public ResponseEntity<?> updateArticle(@PathVariable int articleId, @RequestBody Article article, @RequestParam(required = true) MultipartFile image) throws IOException{
 		User user = userService.getUser(article.getUserId());
 		if(article.getArticleId() == articleId && user != null) {
 			articleService.writeArticle(article, image);
