7fa93de55602274f09ef9702e90fd4adbf2e0c9b
Gardener-Soul <str-pes@naver.com>
2023-11-22 14:48:35 +0900

    articleView 초안 완성

--- SSAFIT_VUEPJT/src/views/articleView.vue
+++ SSAFIT_VUEPJT/src/views/articleView.vue
@@ -1,40 +1,61 @@
 <template>
-    <div class="allArticle-container">
-        <magic-grid>
-            <div class="article" v-for="article in store.articleList">
-                <RouterLink :to="{ name: 'articleDetail', params: { articleId: `${article.articleId}` } }">
-                    <!-- test image -->
-                    <img src="https://source.unsplash.com/random/?workout" alt="" style="width: 90%; height: 200px;">
-                    <div>{{ article.title }}</div>
-                    <div>{{ article.content }}</div>
-                    <div>{{ article.regDate }}</div>
-                    <div>{{ article.checked }}</div>
-                </RouterLink>
-                <button>{{ article.liked }}</button>
-            </div>
-        </magic-grid>
-    </div>
+  <div class="allArticle-container">
+    <magic-grid>
+      <div class="article" v-for="article in articleStore.articleList">
+        <RouterLink
+          :to="{
+            name: 'articleDetail',
+            params: { articleId: `${article.articleId}` },
+          }"
+        >
+          <!-- test image -->
+          <img src="https://source.unsplash.com/random/?workout" alt="" />
+          <div class="main">{{ article.title }}</div>
+          <div class="main">{{ article.content }}</div>
+          <div>{{ article.regDate }}</div>
+        </RouterLink>
+        <button @click="clickHeart(article.articleId)">
+          {{ article.liked }}
+        </button>
+      </div>
+    </magic-grid>
+  </div>
 </template>
 
 <script setup>
-
 import { useArticleStore } from "@/stores/article";
+import { useUsersStore } from "@/stores/users";
 import { RouterView, RouterLink } from "vue-router";
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 
-const store = useArticleStore();
+const articleStore = useArticleStore();
+const userStore = useUsersStore();
 
+const clickHeart = function (articleId) {
+  articleStore.updateHeart(articleId, userStore.loginId);
+};
 
 onMounted(() => {
-    store.getArticleList();
+  articleStore.getArticleList();
 });
 </script>
 
 <style scoped>
 .article {
-    border: 1px solid;
-    border-radius: 2rem;
-    display: flex;
-    flex-direction: column;
+  border: 1px solid;
+  border-radius: 2rem;
+  display: flex;
+  flex-direction: column;
+  width: 12%;
+  /* height: 500px; */
+}
+
+.article img {
+  width: 90%;
+  height: 200px;
+}
+
+.article .main {
+  word-break: break-word;
 }
 </style>
